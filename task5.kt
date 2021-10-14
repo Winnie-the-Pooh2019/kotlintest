8103d3140d4cf6df3c69cc2321f4ce9068325715
Winnie-the-Pooh2019 <gregsonjackk@gmail.com>
2021-10-14 21:55:50 +0700

    #1 5

--- task5.kt
+++ task5.kt
@@ -1,12 +1,3 @@
 fun main(args: Array<String>) {
-
-    val list = args.asList()
-    val argsPairs = list.toHashSet().mapTo(ArrayList()) { Pair(it, list.count { s -> s == it }) }
-
-    argsPairs.sortedWith { l, r ->
-        if (l.second == r.second)
-            l.first.compareTo(r.first)
-        else
-            r.second.compareTo(l.second)
-        }.forEach { println("${it.first} ${it.second}") }
+    args.sorted().groupingBy { it }.eachCount().toList().sortedByDescending { it.second }.forEach { println("${it.first} ${it.second}") }
 }

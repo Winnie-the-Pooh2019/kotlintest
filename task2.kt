1b931823010807a681dc2dc66050d584002e0aa5
Winnie-the-Pooh2019 <gregsonjackk@gmail.com>
2021-10-14 19:51:50 +0700

    #1 2

--- task2.kt
+++ task2.kt
@@ -1,5 +1,3 @@
 fun main(args: Array<String>) {
-    args.asList().asSequence().sortedWith{
-            s1, s2 -> s1.compareTo(s2)
-        }.forEach{s -> println(s)}
+    args.sortedArray().forEach(::println)
 }

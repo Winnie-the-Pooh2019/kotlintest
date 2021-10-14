d532812ae57ab88a7a2ed1cc5fc4beab80206e77
Winnie-the-Pooh2019 <gregsonjackk@gmail.com>
2021-10-14 21:36:26 +0700

    #1 4

--- task4.kt
+++ task4.kt
@@ -1,8 +1,3 @@
 fun main(args: Array<String>) {
-
-    val list = args.asList()
-
-    val argsPairs = list.toHashSet().mapTo(ArrayList()) { Pair(it, list.count { s -> s == it }) }
-
-    argsPairs.sortedWith { l, r -> l.first.compareTo(r.first) }.forEach { println("${it.first} ${it.second}") }
+    args.sorted().groupingBy { it }.eachCount().forEach { println("${it.key} ${it.value}") }
 }

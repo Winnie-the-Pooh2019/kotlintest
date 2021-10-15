af56162383dbd966ceca85d164baf29a902350de
Winnie-the-Pooh2019 <gregsonjackk@gmail.com>
2021-10-15 20:05:18 +0700

    #1 7

--- BUILD.sh
+++ BUILD.sh
@@ -2,4 +2,11 @@
 
 # enter filename without extension as script argument
 
-kotlinc "$1".kt -include-runtime -d "$1".jar
+#kotlinc "$1".kt -include-runtime -d "$1".jar
+
+kotlinc task1.kt -include-runtime -d task1.jar
+kotlinc task2.kt -include-runtime -d task2.jar
+kotlinc task3.kt -include-runtime -d task3.jar
+kotlinc task4.kt -include-runtime -d task4.jar
+kotlinc task5.kt -include-runtime -d task5.jar
+kotlinc task6.kt -include-runtime -d task6.jar

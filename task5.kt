0bb2ba6d7c18695567bfb11bb05f50d5f6955e62
Winnie-the-Pooh2019 <gregsonjackk@gmail.com>
2021-10-16 12:26:12 +0700

    #1 5

--- task5.kt
+++ task5.kt
@@ -1,3 +1,3 @@
 fun main(args: Array<String>) {
-    args.sorted().groupingBy { it }.eachCount().toList().sortedByDescending { it.second }.map { "${it.first} ${it.second}" }.forEach(::println) 
+    args.sorted().groupingBy { it }.eachCount().toList().sortedByDescending { it.second }.map { "${it.first} ${it.second}" }.forEach(::println)
 }

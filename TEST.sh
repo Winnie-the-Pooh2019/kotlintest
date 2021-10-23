ad9ddd8399ebc7a739f23ab5f9821a4cf0d4fc69
Winnie-the-Pooh2019 <gregsonjackk@gmail.com>
2021-10-23 12:07:30 +0700

    test file updated

--- TEST.sh
+++ TEST.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+n=$'\n'
+
 declare -a expected
-expected[1]="the\nquick\nbrown\nfox\njumps\nover\nthe\nlazy\ndog"
-expected[2]="brown\ndog\nfox\njumps\nlazy\nover\nquick\nthe\nthe"
-expected[3]="brown\ndog\nfox\njumps\nlazy\nover\nquick\nthe"
-expected[4]="brown 1\ndog 1\nfox 1\njumps 1\nlazy 1\nover 1\nquick 1\nthe 2"
-expected[5]="the 2\nbrown 1\ndog 1\nfox 1\njumps 1\nlazy 1\nover 1\nquick 1"
-expected[6]="the 2\nbrown 1\ndog 1\nfox 1\njumps 1\nlazy 1\nover 1\nquick 1"
+expected[1]="the${n}quick${n}brown${n}fox${n}jumps${n}over${n}the${n}lazy${n}dog"
+expected[2]="brown${n}dog${n}fox${n}jumps${n}lazy${n}over${n}quick${n}the${n}the"
+expected[3]="brown${n}dog${n}fox${n}jumps${n}lazy${n}over${n}quick${n}the"
+expected[4]="brown 1${n}dog 1${n}fox 1${n}jumps 1${n}lazy 1${n}over 1${n}quick 1${n}the 2"
+expected[5]="the 2${n}brown 1${n}dog 1${n}fox 1${n}jumps 1${n}lazy 1${n}over 1${n}quick 1"
+expected[6]="the 2${n}brown 1${n}dog 1${n}fox 1${n}jumps 1${n}lazy 1${n}over 1${n}quick 1"
 
 orig="the quick brown fox jumps over the lazy dog"
 
@@ -17,6 +19,13 @@ do
   printf "input: %s\n" "$orig"
 
   out="$(java -jar "task$i".jar $orig)"
+
+#  echo "!$out!"
+#  echo "!${expected[$i]}!"
+
+  if [[ "${expected[$i]}" == "$out" ]]; then
+        printf "\nTEST SUCCEED\n"
+  fi
 
   printf "\nactual output:\n\n%s\n" "$out"
 

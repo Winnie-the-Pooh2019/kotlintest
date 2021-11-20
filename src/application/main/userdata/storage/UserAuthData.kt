c0813d79b2026469b3478cf67a7225f7510beacb
Winnie-the-Pooh2019 <gregsonjackk@gmail.com>
2021-11-20 12:10:35 +0700

    #2

--- src/application/main/userdata/storage/UserAuthData.kt
+++ src/application/main/userdata/storage/UserAuthData.kt
@@ -4,12 +4,12 @@ import application.main.userdata.Role
 
 object UserAuthData {
     val usersToRolesToRes = listOf(
-        listOf("Ivan", Role.READ.name, "A.B.C"),
-        listOf("Billy", Role.WRITE.name, "A.B"),
-        listOf("Koala", Role.EXECUTE.name, "A.B.C.D"),
-        listOf("jdoe", Role.READ.name, "a"),
-        listOf("jdoe",Role.WRITE.name, "a.b"),
-        listOf("jrow", Role.EXECUTE.name, "a.b.c"),
-        listOf("jdoe", Role.EXECUTE.name, "a.bc")
+        RoleResourceDTO("Ivan", Role.READ, "A.B.C"),
+        RoleResourceDTO("Billy", Role.WRITE, "A.B"),
+        RoleResourceDTO("Koala", Role.EXECUTE, "A.B.C.D"),
+        RoleResourceDTO("jdoe", Role.READ, "a"),
+        RoleResourceDTO("jdoe",Role.WRITE, "a.b"),
+        RoleResourceDTO("jrow", Role.EXECUTE, "a.b.c"),
+        RoleResourceDTO("jdoe", Role.EXECUTE, "a.bc")
     )
 }

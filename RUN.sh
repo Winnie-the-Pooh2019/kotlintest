0f60b599132f4d752c122c66474bb183111ddf45
Winnie-the-Pooh2019 <gregsonjackk@gmail.com>
2021-11-30 21:49:41 +0700

    bug fixed in run.sh

--- RUN.sh
+++ RUN.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-java -cp ./lib/kotlinx-cli-0.2.1.jar:./out/artifacts/main.jar application.main.MainKt $1
+args=""
+
+for arg in "$@"; do
+  args="${args} ${arg}"
+done
+
+java -cp ./lib/kotlinx-cli-0.2.1.jar:./out/artifacts/main.jar application.main.MainKt $args

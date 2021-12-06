aafcc4fb09e19f5fb4b047ed0d9be568002be916
Winnie-the-Pooh2019 <gregsonjackk@gmail.com>
2021-12-06 21:14:33 +0700

    roadmap3 created

--- src/application/main/main.kt
+++ src/application/main/main.kt
@@ -2,10 +2,10 @@ package application.main
 
 import application.main.domain.Input
 import application.main.services.*
+import org.apache.log4j.LogManager
 import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
-
     val input = Input.parse(args)
 
     val accountProvider = AccountProvider()
